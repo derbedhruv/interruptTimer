9a23967e50082dbffd1a3ddccacf4440218dcaf2
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-12-21 19:04:00 +0530

    added proper comments to trace it again

--- _100Hz_interruptTimer.ino
+++ _100Hz_interruptTimer.ino
@@ -1,6 +1,19 @@
-// http://www.instructables.com/id/Arduino-Timer-Interrupts/
-// will be using a digital lowpass filter to extract DC and output to DAQ
-// declaring variables for dsp implementation
+/* ###########################################################
+  Extracting DC from a signal and then throwing the DC component out via PWM.
+  The PWM is smoothed using an RC filter and then put through a difference amplifier with the original signal
+  THis difference is then sent through analog A3 
+
+  This exercise is one part of building a pulse oximeter, with the LED brightness once again being controlled
+  by PWM (on pin 11, one more LED will need to be added later).
+  
+  Interrupt timer used to implement digital DC filter, taken from
+  http://www.instructables.com/id/Arduino-Timer-Interrupts/
+########################################################### */
+
+#define redLED 11
+// #define irLED 
+#define DCout 3
+
 float filtered_value, last_filtered_value;
 int value, last_value;
 
@@ -8,17 +21,16 @@ void setup(){
   Serial.begin(115200);    // superfast serial communication
   
   double sampling_freq = 200;  // Fsampl in Hz
-  int match_register = 16000000/(1024*sampling_freq) - 1;
+  int match_register = 16000000/(1024*sampling_freq) - 1; // match register to match that sampling frequency
   
-  // STEP 1: we create an interrupt timer at precisely 100Hz
+  // STEP 1: we create an interrupt timer at the sampling frequency
   cli();//stop interrupts
 
-  // set timer1 interrupt at 100Hz
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
   // set compare match register for 1hz increments
-  OCR1A = match_register; // = (16*10^6) / (sampling_freq*1024) - 1 (must be <65536)
+  OCR1A = match_register; // HERE'S WHERE IT'S SET
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
@@ -35,9 +47,9 @@ void setup(){
   TCCR2B = TCCR2B & 0b11111000 | 0x01;  // 31372.55 Hz
 
   // STEP 3 : Give the output pwm value in the range (0, 255) mapped to (0, 5)V
-  pinMode(11, OUTPUT);
+  pinMode(redLED, OUTPUT);
   float voltage = 2.4;  // voltage you wanna outputio
-  analogWrite(11, int((voltage/5.0)*256) - 1);
+  analogWrite(redLED, int((voltage/5.0)*256) - 1);
 }
 
 ISR(TIMER1_COMPA_vect){   //  timer1 interrupt 100Hz
@@ -59,7 +71,7 @@ ISR(TIMER1_COMPA_vect){   //  timer1 interrupt 100Hz
    // the filtered value is now send out through PWM pin D3, which is also controlled by Timer2
    // filtered_value is in the range (0,1023) and the analogWrite value needs to be in the range (0, 255).
    // Hence we convert..
-   analogWrite(3, int(256*(filtered_value + 1)/1024) - 1);
+   analogWrite(DCout, int(256*(filtered_value + 1)/1024) - 1);
    
    // Then we read the value in again from an isolated analog pin and print it out..
    Serial.println(analogRead(A3));
